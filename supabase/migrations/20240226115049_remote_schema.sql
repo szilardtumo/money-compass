f892e4022d1d0aaf9938da2bf4753d3d5f02552a
Szilárd Tumó <tumoszilard@gmail.com>
2024-02-26 16:09:11 +0100

    Updated database seed and migrations.

--- supabase/migrations/20240226115049_remote_schema.sql
+++ supabase/migrations/20240226115049_remote_schema.sql
@@ -33,15 +33,38 @@ CREATE TABLE IF NOT EXISTS "public"."accounts" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL,
     "name" "text" DEFAULT ''::"text" NOT NULL,
-    "user_id" "uuid" NOT NULL
+    "user_id" "uuid" DEFAULT "auth"."uid"() NOT NULL
 );
 
 ALTER TABLE "public"."accounts" OWNER TO "postgres";
 
+CREATE TABLE IF NOT EXISTS "public"."transactions" (
+    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
+    "external_ref" "text",
+    "amount" numeric NOT NULL,
+    "started_date" timestamp with time zone NOT NULL,
+    "completed_date" timestamp with time zone NOT NULL,
+    "description" "text" DEFAULT ''::"text" NOT NULL,
+    "type" "public"."transaction_type" NOT NULL,
+    "subaccount_id" "uuid" NOT NULL,
+    "balance" numeric NOT NULL
+);
+
+ALTER TABLE "public"."transactions" OWNER TO "postgres";
+
+CREATE OR REPLACE VIEW "public"."balances" AS
+ SELECT DISTINCT ON ("transactions"."subaccount_id") "transactions"."subaccount_id",
+    "transactions"."balance"
+   FROM "public"."transactions"
+  ORDER BY "transactions"."subaccount_id", "transactions"."started_date" DESC;
+
+ALTER TABLE "public"."balances" OWNER TO "postgres";
+
 CREATE TABLE IF NOT EXISTS "public"."currencies" (
     "id" "text" NOT NULL,
     "name" "text" NOT NULL,
-    "country" "text" NOT NULL
+    "country" "text" NOT NULL,
+    CONSTRAINT "currencies_id_check" CHECK (("lower"("id") = "id"))
 );
 
 ALTER TABLE "public"."currencies" OWNER TO "postgres";
@@ -58,27 +81,16 @@ CREATE TABLE IF NOT EXISTS "public"."subaccounts" (
     "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
     "account_id" "uuid" NOT NULL,
     "currency" "text" NOT NULL,
-    "value" numeric DEFAULT '0'::numeric NOT NULL,
     "created_at" timestamp with time zone DEFAULT "now"() NOT NULL
 );
 
 ALTER TABLE "public"."subaccounts" OWNER TO "postgres";
 
-CREATE TABLE IF NOT EXISTS "public"."transactions" (
-    "id" "uuid" DEFAULT "gen_random_uuid"() NOT NULL,
-    "external_ref" "text",
-    "amount" numeric NOT NULL,
-    "started_date" timestamp with time zone NOT NULL,
-    "completed_date" timestamp with time zone NOT NULL,
-    "description" "text" DEFAULT ''::"text" NOT NULL,
-    "type" "public"."transaction_type" NOT NULL,
-    "subaccount_id" "uuid"
-);
-
-ALTER TABLE "public"."transactions" OWNER TO "postgres";
-
 ALTER TABLE ONLY "public"."accounts"
     ADD CONSTRAINT "accounts_id_key" UNIQUE ("id");
+
+ALTER TABLE ONLY "public"."accounts"
+    ADD CONSTRAINT "accounts_name_key" UNIQUE ("name");
 
 ALTER TABLE ONLY "public"."accounts"
     ADD CONSTRAINT "accounts_pkey" PRIMARY KEY ("id");
@@ -99,19 +111,34 @@ ALTER TABLE ONLY "public"."accounts"
     ADD CONSTRAINT "accounts_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "auth"."users"("id") ON DELETE CASCADE;
 
 ALTER TABLE ONLY "public"."exchange_rates"
-    ADD CONSTRAINT "exchange_rates_from_fkey" FOREIGN KEY ("from") REFERENCES "public"."currencies"("id");
+    ADD CONSTRAINT "public_exchange_rates_from_fkey" FOREIGN KEY ("from") REFERENCES "public"."currencies"("id") ON UPDATE CASCADE;
 
 ALTER TABLE ONLY "public"."exchange_rates"
-    ADD CONSTRAINT "exchange_rates_to_fkey" FOREIGN KEY ("to") REFERENCES "public"."currencies"("id");
+    ADD CONSTRAINT "public_exchange_rates_to_fkey" FOREIGN KEY ("to") REFERENCES "public"."currencies"("id") ON UPDATE CASCADE;
+
+ALTER TABLE ONLY "public"."subaccounts"
+    ADD CONSTRAINT "public_subaccounts_currency_fkey" FOREIGN KEY ("currency") REFERENCES "public"."currencies"("id") ON UPDATE CASCADE;
+
+ALTER TABLE ONLY "public"."transactions"
+    ADD CONSTRAINT "public_transactions_subaccount_id_fkey" FOREIGN KEY ("subaccount_id") REFERENCES "public"."subaccounts"("id") ON UPDATE CASCADE ON DELETE CASCADE;
 
 ALTER TABLE ONLY "public"."subaccounts"
     ADD CONSTRAINT "subaccounts_account_id_fkey" FOREIGN KEY ("account_id") REFERENCES "public"."accounts"("id") ON UPDATE CASCADE ON DELETE CASCADE;
 
-ALTER TABLE ONLY "public"."subaccounts"
-    ADD CONSTRAINT "subaccounts_currency_fkey" FOREIGN KEY ("currency") REFERENCES "public"."currencies"("id");
+CREATE POLICY "Allow insert based on account_id and accounts.user_id" ON "public"."subaccounts" FOR INSERT WITH CHECK (("account_id" IN ( SELECT "accounts"."id"
+   FROM "public"."accounts"
+  WHERE ("accounts"."user_id" = "auth"."uid"()))));
 
-ALTER TABLE ONLY "public"."transactions"
-    ADD CONSTRAINT "transactions_subaccount_id_fkey" FOREIGN KEY ("subaccount_id") REFERENCES "public"."subaccounts"("id") ON UPDATE CASCADE ON DELETE SET NULL;
+CREATE POLICY "Enable all access for users based on accounts.user_id" ON "public"."transactions" USING (("auth"."uid"() = ( SELECT "accounts"."user_id"
+   FROM ("public"."subaccounts"
+     JOIN "public"."accounts" ON (("accounts"."id" = "subaccounts"."account_id")))
+  WHERE ("subaccounts"."id" = "transactions"."subaccount_id"))));
+
+CREATE POLICY "Enable delete for users based on user_id" ON "public"."accounts" FOR DELETE USING (("auth"."uid"() = "user_id"));
+
+CREATE POLICY "Enable insert for users based on user_id" ON "public"."accounts" FOR INSERT WITH CHECK (("auth"."uid"() = "user_id"));
+
+CREATE POLICY "Enable read access for all users" ON "public"."currencies" FOR SELECT USING (true);
 
 CREATE POLICY "Enable read access for all users" ON "public"."exchange_rates" FOR SELECT USING (true);
 
@@ -140,6 +167,14 @@ GRANT ALL ON TABLE "public"."accounts" TO "anon";
 GRANT ALL ON TABLE "public"."accounts" TO "authenticated";
 GRANT ALL ON TABLE "public"."accounts" TO "service_role";
 
+GRANT ALL ON TABLE "public"."transactions" TO "anon";
+GRANT ALL ON TABLE "public"."transactions" TO "authenticated";
+GRANT ALL ON TABLE "public"."transactions" TO "service_role";
+
+GRANT ALL ON TABLE "public"."balances" TO "anon";
+GRANT ALL ON TABLE "public"."balances" TO "authenticated";
+GRANT ALL ON TABLE "public"."balances" TO "service_role";
+
 GRANT ALL ON TABLE "public"."currencies" TO "anon";
 GRANT ALL ON TABLE "public"."currencies" TO "authenticated";
 GRANT ALL ON TABLE "public"."currencies" TO "service_role";
@@ -151,10 +186,6 @@ GRANT ALL ON TABLE "public"."exchange_rates" TO "service_role";
 GRANT ALL ON TABLE "public"."subaccounts" TO "anon";
 GRANT ALL ON TABLE "public"."subaccounts" TO "authenticated";
 GRANT ALL ON TABLE "public"."subaccounts" TO "service_role";
-
-GRANT ALL ON TABLE "public"."transactions" TO "anon";
-GRANT ALL ON TABLE "public"."transactions" TO "authenticated";
-GRANT ALL ON TABLE "public"."transactions" TO "service_role";
 
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "postgres";
 ALTER DEFAULT PRIVILEGES FOR ROLE "postgres" IN SCHEMA "public" GRANT ALL ON SEQUENCES  TO "anon";
--- supabase/seed.sql
+++ supabase/seed.sql
@@ -9,25 +9,31 @@ SET xmloption = content;
 SET client_min_messages = warning;
 SET row_security = off;
 
-INSERT INTO public.accounts VALUES ('8ad11270-5ee9-4322-b5e0-ef89056ed9b7', '2024-02-05 14:23:47.748568+00', 'Revolut', 'b42f2f4c-0f89-49b7-9eae-25e65566df04');
 
-INSERT INTO public.currencies VALUES ('EUR', 'Euro', 'Europe');
-INSERT INTO public.currencies VALUES ('HUF', 'Hungarian Forint', 'Hungary');
-INSERT INTO public.currencies VALUES ('RON', 'Romanian Leu', 'Romania');
-INSERT INTO public.currencies VALUES ('USD', 'US Dollar', 'United States of America');
+INSERT INTO public.accounts VALUES ('21240762-fdd7-4336-9292-7bc12af37b79', '2024-02-26 11:01:47.818804+00', 'Revolut', 'b42f2f4c-0f89-49b7-9eae-25e65566df04');
 
-INSERT INTO public.exchange_rates VALUES ('EUR', 'RON', 4.98);
-INSERT INTO public.exchange_rates VALUES ('EUR', 'USD', 1.08);
-INSERT INTO public.exchange_rates VALUES ('HUF', 'RON', 0.013);
-INSERT INTO public.exchange_rates VALUES ('HUF', 'USD', 0.0028);
-INSERT INTO public.exchange_rates VALUES ('RON', 'EUR', 0.20);
-INSERT INTO public.exchange_rates VALUES ('RON', 'HUF', 77.92);
-INSERT INTO public.exchange_rates VALUES ('RON', 'USD', 0.22);
-INSERT INTO public.exchange_rates VALUES ('USD', 'EUR', 0.93);
-INSERT INTO public.exchange_rates VALUES ('USD', 'HUF', 360.23);
-INSERT INTO public.exchange_rates VALUES ('USD', 'RON', 4.62);
-INSERT INTO public.exchange_rates VALUES ('EUR', 'HUF', 387.94);
-INSERT INTO public.exchange_rates VALUES ('HUF', 'EUR', 0.0026);
+INSERT INTO public.currencies VALUES ('eur', 'Euro', 'Europe');
+INSERT INTO public.currencies VALUES ('huf', 'Hungarian Forint', 'Hungary');
+INSERT INTO public.currencies VALUES ('ron', 'Romanian Leu', 'Romania');
+INSERT INTO public.currencies VALUES ('usd', 'US Dollar', 'United States of America');
 
-INSERT INTO public.subaccounts VALUES ('62b36689-cae6-46f6-9870-5df6877d6649', '8ad11270-5ee9-4322-b5e0-ef89056ed9b7', 'EUR', 10, '2024-02-07 11:25:15.743791+00');
-INSERT INTO public.subaccounts VALUES ('f3386e7c-4c86-48f3-9c8f-ac99efab4609', '8ad11270-5ee9-4322-b5e0-ef89056ed9b7', 'HUF', 100, '2024-02-07 11:26:45.209338+00');
+INSERT INTO public.exchange_rates VALUES ('huf', 'eur', 0.0026);
+INSERT INTO public.exchange_rates VALUES ('eur', 'huf', 387.94);
+INSERT INTO public.exchange_rates VALUES ('ron', 'eur', 0.20);
+INSERT INTO public.exchange_rates VALUES ('ron', 'huf', 77.92);
+INSERT INTO public.exchange_rates VALUES ('eur', 'ron', 4.98);
+INSERT INTO public.exchange_rates VALUES ('huf', 'ron', 0.013);
+INSERT INTO public.exchange_rates VALUES ('usd', 'eur', 0.93);
+INSERT INTO public.exchange_rates VALUES ('usd', 'huf', 360.23);
+INSERT INTO public.exchange_rates VALUES ('usd', 'ron', 4.62);
+INSERT INTO public.exchange_rates VALUES ('eur', 'usd', 1.08);
+INSERT INTO public.exchange_rates VALUES ('huf', 'usd', 0.0028);
+INSERT INTO public.exchange_rates VALUES ('ron', 'usd', 0.22);
+
+INSERT INTO public.subaccounts VALUES ('65015151-a755-48a7-acd1-a82b2908a2fb', '21240762-fdd7-4336-9292-7bc12af37b79', 'huf', '2024-02-26 11:01:48.091268+00');
+
+INSERT INTO public.transactions VALUES ('4dc21df9-7cdf-43a6-a06b-f8018eb8f6f5', NULL, 122, '2024-02-26 11:02:46.071+00', '2024-02-26 11:02:46.071+00', '', 'other', '65015151-a755-48a7-acd1-a82b2908a2fb', 122);
+INSERT INTO public.transactions VALUES ('a4309fd4-3b87-4f9c-988c-46d46f008aff', NULL, 33333, '2024-02-26 11:02:52.398+00', '2024-02-26 11:02:52.398+00', '', 'other', '65015151-a755-48a7-acd1-a82b2908a2fb', 33455);
+INSERT INTO public.transactions VALUES ('66da44f3-6637-42c9-9992-c58719af0b03', NULL, 10000, '2024-02-26 11:03:01.283+00', '2024-02-26 11:03:01.283+00', '', 'card_payment', '65015151-a755-48a7-acd1-a82b2908a2fb', 43455);
+INSERT INTO public.transactions VALUES ('dd7b85fb-564f-46d3-a9af-f15feeaab37e', NULL, -1000, '2024-02-26 11:03:24.709+00', '2024-02-26 11:03:24.709+00', '', 'other', '65015151-a755-48a7-acd1-a82b2908a2fb', 42455);
+
