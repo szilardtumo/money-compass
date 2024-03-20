e03d9e63df46227379cdcfdd589ac2b2b139c3f9
Szilárd Tumó <tumoszilard@gmail.com>
2024-03-20 14:12:23 +0100

    Added Separator to not found page.

--- src/app/dashboard/not-found.tsx
+++ src/app/dashboard/not-found.tsx
@@ -1,8 +1,10 @@
+import { Separator } from '@/components/ui/separator';
+
 export default function NotFound() {
   return (
     <main className="flex items-center justify-center h-screen gap-6">
       <h1 className="font-medium tracking-tight text-2xl">404</h1>
-      <div className="h-12 w-px bg-neutral-600" />
+      <Separator decorative orientation="vertical" className="h-12" />
       <h2 className="text-sm">This page could not be found.</h2>
     </main>
   );
