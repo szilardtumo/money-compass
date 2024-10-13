ac13ab6c8c04adc2dc378bd5ff5ab72c77a3243e
Szilárd Tumó <tumoszilard@gmail.com>
2024-10-13 21:43:18 +0200

    Remove tailwindcss forms

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,3 @@
-import tailwindcssForms from '@tailwindcss/forms';
 import type { Config } from 'tailwindcss';
 import { fontFamily } from 'tailwindcss/defaultTheme';
 import tailwindcssAnimate from 'tailwindcss-animate';
@@ -86,36 +85,7 @@ const config = {
       },
     },
   },
-  safelist: [
-    {
-      pattern:
-        /(bg-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
-      variants: ['hover', 'ui-selected'],
-    },
-    {
-      pattern:
-        /(text-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
-      variants: ['hover', 'ui-selected'],
-    },
-    {
-      pattern:
-        /(border-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
-      variants: ['hover', 'ui-selected'],
-    },
-    {
-      pattern:
-        /(ring-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
-    },
-    {
-      pattern:
-        /(stroke-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
-    },
-    {
-      pattern:
-        /(fill-(?:slate|gray|zinc|neutral|stone|red|orange|amber|yellow|lime|green|emerald|teal|cyan|sky|blue|indigo|violet|purple|fuchsia|pink|rose)-(?:50|100|200|300|400|500|600|700|800|900|950))/,
-    },
-  ],
-  plugins: [tailwindcssForms, tailwindcssAnimate],
+  plugins: [tailwindcssAnimate],
 } satisfies Config;
 
 export default config;
