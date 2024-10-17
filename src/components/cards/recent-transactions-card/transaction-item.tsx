6fbf1d82e4d9bdd95b722fe868f418b8b05abcfe
Szilárd Tumó <tumoszilard@gmail.com>
2024-10-17 22:30:17 +0200

    Add edit button to recent transactions card

--- src/components/cards/recent-transactions-card/transaction-item.tsx
+++ src/components/cards/recent-transactions-card/transaction-item.tsx
@@ -1,4 +1,15 @@
+import { DotsHorizontalIcon } from '@radix-ui/react-icons';
+import Link from 'next/link';
+
+import { useUpdateTransactionDialog } from '@/components/providers/update-transaction-dialog-provider';
 import { Avatar } from '@/components/ui/avatar';
+import { Button } from '@/components/ui/button';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from '@/components/ui/dropdown-menu';
 import { NavLink } from '@/components/ui/nav-link';
 import { TransactionWithAccount } from '@/lib/types/transactions.types';
 import { formatCurrency, formatTime } from '@/lib/utils/formatters';
@@ -8,6 +19,8 @@ interface TransactionItemProps {
 }
 
 export function TransactionItem({ transaction }: TransactionItemProps) {
+  const { openDialog: openUpdateTransactionDialog } = useUpdateTransactionDialog();
+
   return (
     <div className="flex items-start gap-4">
       <Avatar className="hidden sm:flex h-9 w-9 border" />
@@ -28,6 +41,30 @@ export function TransactionItem({ transaction }: TransactionItemProps) {
       <div className="ml-auto font-medium whitespace-nowrap">
         {formatCurrency(transaction.amount.originalValue, transaction.originalCurrency)}
       </div>
+      <DropdownMenu>
+        <DropdownMenuTrigger asChild>
+          <Button variant="ghost" size="icon">
+            <span className="sr-only">Open menu</span>
+            <DotsHorizontalIcon className="size-4" />
+          </Button>
+        </DropdownMenuTrigger>
+        <DropdownMenuContent align="end">
+          <DropdownMenuItem
+            onClick={() =>
+              openUpdateTransactionDialog({
+                ...transaction,
+                amount: transaction.amount.originalValue,
+                currency: transaction.originalCurrency,
+              })
+            }
+          >
+            Edit
+          </DropdownMenuItem>
+          <DropdownMenuItem asChild>
+            <Link href="/dashboard/transactions">Details</Link>
+          </DropdownMenuItem>
+        </DropdownMenuContent>
+      </DropdownMenu>
     </div>
   );
 }
