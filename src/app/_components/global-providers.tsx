c940025d9ee5fd27f5c2fac7eeafc7de4e3d3058
Szilárd Tumó <tumoszilard@gmail.com>
2024-12-12 19:18:04 +0100

    Remove client code provider

--- src/app/_components/global-providers.tsx
+++ src/app/_components/global-providers.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'next-themes';
 
-import { ClientCodeProvider } from '@/components/providers/client-code-provider';
 import { CreateTransactionDialogProvider } from '@/components/providers/create-transaction-dialog-provider';
 import { LoadingProvider } from '@/components/providers/loading-provider';
 import { UpdateTransactionDialogProvider } from '@/components/providers/update-transaction-dialog-provider';
@@ -9,18 +8,16 @@ import { TooltipProvider } from '@/components/ui/tooltip';
 
 export function GlobalProviders({ children }: { children: React.ReactNode }) {
   return (
-    <ClientCodeProvider>
-      <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
-        <LoadingProvider>
-          <TooltipProvider>
-            <CreateTransactionDialogProvider>
-              <UpdateTransactionDialogProvider>
-                <UpsertAccountDialogProvider>{children}</UpsertAccountDialogProvider>
-              </UpdateTransactionDialogProvider>
-            </CreateTransactionDialogProvider>
-          </TooltipProvider>
-        </LoadingProvider>
-      </ThemeProvider>
-    </ClientCodeProvider>
+    <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
+      <LoadingProvider>
+        <TooltipProvider>
+          <CreateTransactionDialogProvider>
+            <UpdateTransactionDialogProvider>
+              <UpsertAccountDialogProvider>{children}</UpsertAccountDialogProvider>
+            </UpdateTransactionDialogProvider>
+          </CreateTransactionDialogProvider>
+        </TooltipProvider>
+      </LoadingProvider>
+    </ThemeProvider>
   );
 }
