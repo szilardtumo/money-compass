5033f8811e625b30778b73a0ef6cb3663054c8b2
Szilárd Tumó <tumoszilard@gmail.com>
2025-04-15 20:22:18 +0200

    fix: fix export issues for server components

--- src/components/dialogs/dialogs-provider.tsx
+++ src/components/dialogs/dialogs-provider.tsx
@@ -1,17 +1,11 @@
 import { Suspense } from 'react';
 
-import {
-  CreateTransactionDialog,
-  CreateTransactionDialogProvider,
-} from '@/components/dialogs/create-transaction-dialog';
-import {
-  UpdateTransactionDialog,
-  UpdateTransactionDialogProvider,
-} from '@/components/dialogs/update-transaction-dialog';
-import {
-  UpsertAccountDialog,
-  UpsertAccountDialogProvider,
-} from '@/components/dialogs/upsert-account-dialog';
+import { CreateTransactionDialogProvider } from '@/components/dialogs/create-transaction-dialog';
+import { CreateTransactionDialog } from '@/components/dialogs/create-transaction-dialog/create-transaction-dialog';
+import { UpdateTransactionDialogProvider } from '@/components/dialogs/update-transaction-dialog';
+import { UpdateTransactionDialog } from '@/components/dialogs/update-transaction-dialog/update-transaction-dialog';
+import { UpsertAccountDialogProvider } from '@/components/dialogs/upsert-account-dialog';
+import { UpsertAccountDialog } from '@/components/dialogs/upsert-account-dialog/upsert-account-dialog';
 
 export function DialogsProvider({ children }: { children: React.ReactNode }) {
   return (
