e23f025ab3d755b071746e764870e45da87e0c06
Szilárd Tumó <tumoszilard@gmail.com>
2025-07-25 18:24:46 +0200

    Fix auth error

--- src/app/_components/global-providers.tsx
+++ src/app/_components/global-providers.tsx
@@ -1,6 +1,5 @@
 import { ThemeProvider } from 'next-themes';
 
-import { DialogsProvider } from '@/components/dialogs/dialogs-provider';
 import { LoadingProvider } from '@/components/providers/loading-provider';
 import { BrowserSupabaseClientProvider } from '@/components/providers/supabase-client-provider';
 import { TooltipProvider } from '@/components/ui/tooltip';
@@ -10,9 +9,7 @@ export function GlobalProviders({ children }: { children: React.ReactNode }) {
     <ThemeProvider attribute="class" defaultTheme="system" enableSystem disableTransitionOnChange>
       <BrowserSupabaseClientProvider>
         <LoadingProvider>
-          <TooltipProvider>
-            <DialogsProvider>{children}</DialogsProvider>
-          </TooltipProvider>
+          <TooltipProvider>{children}</TooltipProvider>
         </LoadingProvider>
       </BrowserSupabaseClientProvider>
     </ThemeProvider>
--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,12 +1,26 @@
+import { redirect } from 'next/navigation';
+
+import { DialogsProvider } from '@/components/dialogs/dialogs-provider';
 import { PageHeaderSlot, PageHeaderSlotProvider } from '@/components/ui/page-header-slot';
 import { PageContent, PageHeader, PageLayout } from '@/components/ui/page-layout';
 import { ResizablePanel, ResizablePanelGroup } from '@/components/ui/resizable';
+import { AuthenticationError } from '@/lib/api/errors';
+import { apiQueries } from '@/server/api/queries';
 
 import { Navbar } from './_components/navbar';
 
-export default function DashboardLayout({ children }: { children: React.ReactNode }) {
+export default async function DashboardLayout({ children }: { children: React.ReactNode }) {
+  try {
+    await apiQueries.profiles.getProfile();
+  } catch (error) {
+    if (error instanceof AuthenticationError) {
+      redirect('/auth/login');
+    }
+    throw error;
+  }
+
   return (
-    <>
+    <DialogsProvider>
       <ResizablePanelGroup direction="horizontal" className="h-full min-h-screen items-stretch">
         <Navbar />
         <ResizablePanel id="main-panel" defaultSize={80}>
@@ -21,6 +35,6 @@ export default function DashboardLayout({ children }: { children: React.ReactNod
           </PageLayout>
         </ResizablePanel>
       </ResizablePanelGroup>
-    </>
+    </DialogsProvider>
   );
 }
